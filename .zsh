71ef669a976d1c4894fe09ab674455fe74bcfdfa
Sahil Gupta <sahilgupta@MacBook.local>
2020-08-23 04:50:23 +0530

    minor change

---
+++ .zsh
@@ -0,0 +1,7 @@
+# Show current directory (current branch)
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+setopt PROMPT_SUBST
+PROMPT='%9c%{%F{green}%}$(parse_git_branch)%{%F{none}%} $ '
